a65b83287c509c7f9176709e9a6e3124db22f902
vlad_saltanov <saltanovvlad@yandex.ru>
2024-08-09 16:00:06 +0000

    Goes to hh.ru, changed default viewPort

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import puppeteer from 'puppeteer';
 // Or import puppeteer from 'puppeteer-core';
 
 // Launch the browser and open a new blank page
-const browser = await puppeteer.launch({devtools: true});
+const browser = await puppeteer.launch({devtools: true, defaultViewport: null}); //Добавил хуйню чтобы на весь экран было
 const page = await browser.newPage();
 
 // Navigate the page to a URL.
-await page.goto('https://developer.chrome.com/');
+await page.goto('https://hh.ru/');
 
 // Set screen size.
-await page.setViewport({width: 1080, height: 1024});
+//await page.setViewport({width: 1900, height: 1070}); Нам это не надо
 
 // Type into search box.
 await page.locator('.devsite-search-field').fill('automate beyond recorder');
