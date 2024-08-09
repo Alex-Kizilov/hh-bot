60211c5629e5581636d50dac1730954fd0fed1d1
Рыков Алексей Алексеевич <rykov311@gmail.com>
2024-08-09 18:37:19 +0300

    Добавлен запуск

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 // Or import puppeteer from 'puppeteer-core';
 
 // Launch the browser and open a new blank page
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({devtools: true});
 const page = await browser.newPage();
 
 // Navigate the page to a URL.
