8a6fc06a10eac57b11c649610bddbba2ad8c7613
Рыков Алексей Алексеевич <rykov311@gmail.com>
2024-08-09 19:42:08 +0300

    test find vacancy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import puppeteer from 'puppeteer';
 import dotenv from 'dotenv';
 
+function removeAreaParam(url: string): string {
+    // Создаем объект URL из строки
+    const parsedUrl = new URL(url);
+
+    // Удаляем параметр 'area' из URLSearchParams
+    parsedUrl.searchParams.delete('area');
+
+    // Возвращаем обновленный URL как строку
+    return parsedUrl.toString();
+}
+
+
 // Загрузите переменные окружения из файла .env
 dotenv.config();
 // Or import puppeteer from 'puppeteer-core';
@@ -16,11 +28,13 @@ const value = process.env.HH_TOKEN_VALUE || '';
 await page.setCookie({domain, name, value});
 
 // Navigate the page to a URL.
-await page.goto('https://hh.ru/');
+await page.goto('https://hh.ru/', {timeout: 0});
 
-await page.focus('#a11y-search-input');
+await page.focus('input[id=a11y-search-input]');
 await page.keyboard.type('Frontend');
 await page.keyboard.press('Enter')
+
+await page.goto(removeAreaParam(page.url()));
 
 // await browser.close();
 
